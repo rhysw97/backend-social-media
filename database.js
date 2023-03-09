33aa2c437c064bd2f430f844ce3c4145638e1bee
rhysw97 <rhysw97@gmail.com>
2023-03-09 17:01:01 +0000

    added backend logic to see if email and username were taken

--- database.js
+++ database.js
@@ -10,22 +10,39 @@ class Database {
         console.log(result)
     }
     async addUserToDatabase(databaseName, collectionName, data) {
-       
+        const errors = {
+            email: false,
+            username: false,
+        }
         try{
             await this.client.connect();
-    
-            const added = await this.client.db(databaseName).collection(collectionName).insertOne(data)
-            console.log(`New user created with the following username: ${added.insertedId}`)
-            const users = []
-            this.client.db(databaseName).collection(collectionName).find({}).toArray((err, result) => {
-                if (err) throw err
-                console.log(result)
-            })
-            console.log(users)
+            
+            const emailCheck  = await this.client.db(databaseName).collection('Users').findOne({email: data.email})
+            const usernameCheck  = await this.client.db(databaseName).collection('Users').findOne({username: data.username})
+            console.log(usernameCheck)
+            if(emailCheck.email === data.email) {
+                errors.email = true
+            } else {
+                errors.email = false
+            }
+
+            if(usernameCheck.username === data.username) {
+                errors.username = true
+            } else {
+                errors.username = false
+            }
+
+            if(!errors.username || !errors.email) {
+                const added = await this.client.db(databaseName).collection(collectionName).insertOne(data)
+            } else {
+                console.log("Not add due to duplicate")
+            }
+
+            return errors
+        
         } catch(e) {
-           console.error(e)
+            console.error(e)
         }
-      
     }
 
     async listDatabases() {
