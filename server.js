3c59e4c415e25b01d719489a72e4895c1104ef44
rhysw97 <rhysw97@gmail.com>
2023-03-16 18:28:08 +0000

    added sessions storage

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const Database = require('./database');
 const { restart } = require('nodemon');
-const sessions=require('express-session')
+const session=require('express-session')
 //YGZxR5P9sFV13v8c
 
 const db = new Database.Database("mongodb+srv://rhysw97:7jv51e8bzb4jg0xP@cluster0.jx0jttw.mongodb.net/?retryWrites=true&w=majority")
@@ -10,17 +10,30 @@ const db = new Database.Database("mongodb+srv://rhysw97:7jv51e8bzb4jg0xP@cluster
 const app = express();
 require("dotenv").config();
 
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
+    credentials: true,
+}))
+
 app.use(express.json())
-app.use(sessions({
-    secret: 'this is a secret key',
+app.use(session({
+    secret: process.env.SESSION_SECRET,
     saveUninitialized: true,
-    cookie: {maxAge: 1000*60*60},
     resave: false,
+    cookie: {
+        httpOnly: true,
+        maxAge: parseInt(process.env.SESSION_MAX_AGE)
+    },
 }))
 
 const cookieParser=require('cookie-parser')
 app.use(cookieParser())
+
+app.use((req, res, next) => {
+    console.log(req.session);
+    next();
+})
 
 const port = process.env.PORT
 app.get('/', (req, res) => {
@@ -48,16 +61,19 @@ async function waitForLoginDetails(data, response, request) {
 app.post('/register', (req, res) => {
     const body = req.body
     console.log(body)
-    createUser(res, body)
+    createUser(res, req, body)
     //check if username or email is taken
     //if it is send back to the client to display to user
     //if not create new user and send back to client that user has been created and move onto profile set up page 
     //may complete profile set up page later and sort out user post system first //my guys this seems to be going okay at least for now
 })
 
-async function createUser(response, data) {
+async function createUser(response, request, data) {
     const checks = await db.addUserToDatabase('gig-mates', 'Users', data)
     console.log(checks)
+    if(checks) {
+        request.session.username = data.username
+    }
     response.send(JSON.stringify(checks))
 }
 
@@ -66,6 +82,13 @@ app.get('/register', (req, res) => {
     res.send('register')
 })
 
+app.post('/logout', (request, response) => {
+    request.session.destroy();
+})
+
+app.post('/posts', (request, response) => {
+    console.log(request.session.username)
+})
 
 app.listen(port, () =>{
         console.log(`App listening on port ${port}!`)
