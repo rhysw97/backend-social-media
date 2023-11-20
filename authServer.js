738913d4235a74f2541f13f12c2b7f10bfb2392b
rhysw97 <rhysw97@gmail.com>
2023-11-20 16:24:06 +0000

    Revert "updated auth"
    This reverts commit 24311ce5612e5a7793492abb0be4b8ef4b9fad1f.

--- authServer.js
+++ authServer.js
@@ -1,70 +1,50 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://rhysw97:7jv51e8bzb4jg0xP@cluster0.jx0jttw.mongodb.net/?retryWrites=true&w=majority")
 
 const jwt = require('jsonwebtoken')
 
 app.use(express.json())
 
-let refreshTokens = []
-
-app.post('/token', (req, res) => {
-    const refreshToken = req.body.token
-
-    if(refreshToken == null) return res.sendStatus(401)
-    if(!refreshTokens.includes(refreshTokens)) return res.sendStatus(403)
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-        if(err) return res.sendStatus(403)
-        const accessToken = generateAccessToken({name: user.name})
-    res.json({accessToken: accessToken})
-    })
-})
-
-app.post('/login', (request, response) => {
-    //Authenicate User
-    const body = request.body
-    waitForLoginDetails(body, response)
-
-})
-
-async function waitForLoginDetails(data, response) {
-    const loginData = await checkLoginDetails(data);
-
-    if(loginData.accepted) {
-        const accessToken = generateAccessToken(loginData.username)
-
-        const refreshToken = jwt.sign({name: loginData.username}, process.env.REFRESH_TOKEN_SECRET)
-        
-        return response.send({
-            username: request.session.username,
-            loggedin: true,
-            accessToken: accessToken, 
-            refreshToken: refreshToken,
-        })
-
-
-    } else {
-        response.send({loggedin:false})
-    }  
-}
-
-async function checkLoginDetails(data) {
-    const userData = await mongoose.connection.db.collection('Users', (err, collection) => {
-        console.log(collection.findOne({email: data.email}))
-    })
-    console.log(userData)
-    if(userData) {
-        if(userData.password === data.password) {
-            return {accepted: true, username: userData.username}
-        }
+const posts = [
+    {
+        username: 'Rhys',
+        title: 'Post 1'
+    },
+    {
+        username: 'Jim',
+        title: 'Post 2'
     }
-    return {accepted: false, username: ''}
-}
+]
 
-function generateAccessToken(user) {
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '15s'})
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name))
+})
+
+app.post('/login', (req, res) => {
+    //Authenicate User
+
+    const username = req.body.username
+    const user = { name: username }
+    
+    const accessToken = jwt.sign(user, process.env.JWT_SECRET)
+    res.json({accessToken: accessToken})
+
+})
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    
+    const token = authHeader && authHeader.split(' ')[1]
+    console.log(token)
+    if(token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+
+        req.user = user
+        next()
+    })
 }
 
 app.listen(4000)
